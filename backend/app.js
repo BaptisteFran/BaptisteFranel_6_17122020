e2e092b4d0ea4dff8aba2995b4d0d7123961f9b6
Kerstern <baptiste.franel@gmail.com>
2021-03-31 11:28:28 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    res.setHeader('Content-Security-Policy', "default-src 'none'; font-src 'none'; img-src 'none'; script-src 'none'; style-src 'none'; frame-src 'none'");
+    res.setHeader('Content-Security-Policy', "default-src 'none'");
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    req.withCredentials = true;
     next();
 })
 
