81b261eaceb2db9c0efdd1a261b52a5ade5846a7
Kerstern <baptiste.franel@gmail.com>
2021-04-01 07:52:07 +0200

    Update
    - Ajout Helmet pour plus de sécurité

--- backend/app.js
+++ backend/app.js
@@ -3,15 +3,17 @@ const mongoose = require('mongoose');
 const saucesRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 const path = require('path');
+const helmet = require("helmet");
 
 MY_APP_SECRET = process.env.APP_SECRET;
 
 
 mongoose.connect(MY_APP_SECRET,
-{
-    
-    useNewUrlParser: true,
-    useUnifiedTopology: true})
+    {
+
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongDB échouée !'));
 
@@ -21,14 +23,12 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    res.setHeader('Content-Security-Policy', "default-src 'none'");
-    res.setHeader('Content-Type', 'application/json; charset=utf-8');
-    req.withCredentials = true;
     next();
 })
 
 
-
+app.use(helmet());
+app.disable("x-powered-by");
 app.use(express.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
