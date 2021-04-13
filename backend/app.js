e67546c57f0c0ea56450b4571ebcaaa13b1adbe0
Kerstern <baptiste.franel@gmail.com>
2021-04-13 15:09:31 +0200

    Update
    - Ajout de différents middleware OWASP

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const saucesRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 const path = require('path');
+const cors = require('cors');
 const helmet = require("helmet");
 
 MY_APP_SECRET = process.env.APP_SECRET;
@@ -19,21 +20,25 @@ mongoose.connect(MY_APP_SECRET,
 
 const app = express();
 
+var corsOptions = {
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
 })
-
 
 app.use(helmet());
 app.disable("x-powered-by");
 app.use(express.json());
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use('/api/sauces', saucesRoutes);
-app.use('/api/auth', userRoutes);
+app.use('/images', cors(corsOptions), express.static(path.join(__dirname, 'images')));
+app.use('/api/sauces', cors(corsOptions), saucesRoutes);
+app.use('/api/auth', cors(corsOptions), userRoutes);
 
 
 module.exports = app;
